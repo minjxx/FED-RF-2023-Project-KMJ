0b8a3fddf4248dbfc8657d53a0f0d1f32b4da80b
minjxx <qnsdbtlrp1018@naver.com>
2023-09-05 17:44:46 +0900

    uppdate

---
+++ 01.Seoul_WallPaper_PJ/03.구현소스/js/main.js
@@ -0,0 +1,8 @@
+/* main.js */
+
+/********************************************* 
+    함수 : scrTopBtn
+    기능 : 사이트 탑버튼 (클릭시 페이지 최상단으로 이동)
+*********************************************/
+let scrTopBtn = document.querySelector('#topBtn');
+console.log('탑버튼:',scrTopBtn);
