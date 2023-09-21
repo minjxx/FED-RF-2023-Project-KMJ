1d0bd820431b6eaebd94a885eea7c2fca99b7d48
minjxx <qnsdbtlrp1018@naver.com>
2023-09-21 10:09:02 +0900

    up

--- 01.Seoul_WallPaper_PJ/03.구현소스/js/main.js
+++ 01.Seoul_WallPaper_PJ/03.구현소스/js/main.js
@@ -1,26 +1,38 @@
 /* main.js */
 
-// DOM 함수 객체 //////////////
-const domFn = {
-    // 요소선택함수 ////////
-    qs: (x) => document.querySelector(x),
-    qsEl: (el, x) => el.querySelector(x),
-    qsa: (x) => document.querySelectorAll(x),
-    qsaEl: (el, x) => el.querySelectorAll(x),
+// 모듈 불러오기 ///////
+// DOM함수 모듈
+import dFn from "./dom.js";
+// 부드러운 스크롤 모듈
+import { startSS, setPos } from "./smoothScroll23.js";
 
-    // 이벤트셋팅함수
-    addEvt: (ele, evt, fn) => ele.addEventListener(evt, fn),
 
-    // 바운딩top값 리턴함수
-    getBCR: (ele) => ele.getBoundingClientRect().top,
-}; /////// domFn 객체 /////////////
+// 부드러운 스크롤 적용 //////////
+startSS();
+
+// 0. 새로고치면 스크롤바 위치캐싱후 맨위로 이동
+setTimeout(() => {
+  // 윈도우 스크롤 맨위로!
+  window.scrollTo(0, 0);
+  // 부드러운 스크롤 위치값 반영!
+  setPos(0);
+  // 안하면 원래 위치로 스크롤시 튐!
+}, 400);
+// 0. 스크롤바 트랙을 잡고 위치이동시 위치값 반영
+dFn.addEvt(window, "mouseup", () => setPos(window.scrollY));
+//////// mouseup /////////////
+
+// 0. 키보드 방향키 이동시 위치값 반영
+dFn.addEvt(window, "keyup", () => setPos(window.scrollY));
+//////// mouseup /////////////
+
 
 
 /********************************************* 
     함수 : scrTopBtn
     기능 : 사이트 탑버튼 (클릭시 페이지 최상단으로 이동)
 *********************************************/
-let scrTopBtn = domFn.qs("#topBtn");
+let scrTopBtn = dFn.qs("#topBtn");
 // console.log('탑버튼:',scrTopBtn);
 
 scrTopBtn.addEventListener("click", (e) => {
@@ -33,22 +45,22 @@ scrTopBtn.addEventListener("click", (e) => {
 
 
 // [ gnb메뉴에 마우스 오버시 header에 .bgWhite 클래스 넣어서 흰색배경 나오게하기 ]
-const gnbHover = domFn.qs("#gnb");
-const headerBgChg = domFn.qs("#header");
+const gnbHover = dFn.qs("#gnb");
+const headerBgChg = dFn.qs("#header");
 
-domFn.addEvt(gnbHover,'mouseover',()=>{
+dFn.addEvt(gnbHover,'mouseover',()=>{
     headerBgChg.classList.add('bgWhite');
 });
-domFn.addEvt(gnbHover,'mouseleave',()=>{
+dFn.addEvt(gnbHover,'mouseleave',()=>{
     headerBgChg.classList.remove('bgWhite');
 });
 //////////////////////////////////////////////////////
 
 
 // [ 메인 검색 버튼 클릭시 .on 클래스 넣어서 검색영역 input 넓어지게하기 ]
-const topSrchBtn = domFn.qs(".serach-icon");
-const srchWidth = domFn.qs(".search-box .area");
-domFn.addEvt(topSrchBtn,'click',()=>{
+const topSrchBtn = dFn.qs(".serach-icon");
+const srchWidth = dFn.qs(".search-box .area");
+dFn.addEvt(topSrchBtn,'click',()=>{
     srchWidth.classList.add('on');
     topSrchBtn.classList.toggle('on');
 });
@@ -56,17 +68,17 @@ domFn.addEvt(topSrchBtn,'click',()=>{
 
 
 // [ 햄버거버튼 클릭시 오른쪽에서 메뉴 노출 ]
-const allMenuBtn = domFn.qs(".allMenuBtn");
-const allMenuWrap = domFn.qs(".allMenuWrap");
-const layerBg = domFn.qs(".allMenuWrap .layerBg");
-const rightBox = domFn.qs(".right-box");
+const allMenuBtn = dFn.qs(".allMenuBtn");
+const allMenuWrap = dFn.qs(".allMenuWrap");
+const layerBg = dFn.qs(".allMenuWrap .layerBg");
+const rightBox = dFn.qs(".right-box");
 
-domFn.addEvt(allMenuBtn,'click',()=>{
+dFn.addEvt(allMenuBtn,'click',()=>{
     allMenuWrap.classList.toggle('on');
     allMenuBtn.classList.toggle('on');
     rightBox.classList.toggle('on');
 });
-domFn.addEvt(layerBg,'click',()=>{
+dFn.addEvt(layerBg,'click',()=>{
     allMenuWrap.classList.toggle('on');
     allMenuBtn.classList.toggle('on');
     rightBox.classList.toggle('on');
@@ -75,7 +87,7 @@ domFn.addEvt(layerBg,'click',()=>{
 
 
 // [ 오른쪽 메뉴에 하위메뉴 노출 ]
-const allMenuList = domFn.qsa(".allMenuWrap .allMenu > li");
+const allMenuList = dFn.qsa(".allMenuWrap .allMenu > li");
 
 for (let x of allMenuList) {
     // console.log('x:',x);
@@ -112,11 +124,11 @@ for (let x of allMenuList) {
 // 이벤트 대상: window
 // 이벤트 종류: scroll
 // 위치대상: .tpg -> 스티키박스를 싸고있는 부모박스
-const tpg = domFn.qs(".con1");
+const tpg = dFn.qs(".con1");
 // 움직일대상: .slidePg>ul
-const target = domFn.qs(".slidePg>ul");
+const target = dFn.qs(".slidePg>ul");
 // 고정시키는 마지막대상 : .slidePg .search-wrap
-const searchSlide = domFn.qs(".slidePg .search-card");
+const searchSlide = dFn.qs(".slidePg .search-card");
 
 // 광휠상태변수(0-허용,1-금지)
 let stsWheel = 0;
@@ -128,7 +140,7 @@ let numWheel = 0;
 let winH = window.innerHeight;
 
 // 윈도우 이벤트 걸기
-domFn.addEvt(window, "scroll", moveSlide);
+dFn.addEvt(window, "scroll", moveSlide);
 
 function moveSlide() {
 
@@ -138,7 +150,7 @@ function moveSlide() {
     setTimeout(()=>stsWheel=0,TIME_WHEEL); // 해제
 
     // 1. 스티키 부모박스 바운딩top값
-    let bTop = domFn.getBCR(tpg);
+    let bTop = dFn.getBCR(tpg);
     // console.log('나야나!',bTop);
 
     // 2. 바운딩값으로  대상 left위치 변경하기
@@ -171,7 +183,7 @@ function moveSlide() {
 // [ 따라다니는 마우스커서 ] ///////////////////////////////
 // 1. 대상선정 : 
 // (1) 움직일 대상 : .cuser-mover
-const mover = domFn.qs('.cuser-mover');
+const mover = dFn.qs('.cuser-mover');
 // (2) 이벤트 대상 : document.body
 const myBody = document.body;
 // 무버 크기의 절반계산
@@ -210,7 +222,7 @@ myBody.onmouseleave = () => {
 
 // [3] a요소에 오버시 원 크게만들기
 // 대상 : .link
-const link = domFn.qsa('a');
+const link = dFn.qsa('a');
 
 // 한번에 셋팅하기
 link.forEach(ele=>{
@@ -221,7 +233,7 @@ link.forEach(ele=>{
 }); // forEach
 
 // 푸터 영역만 마우스 커서 색상 변경
-const footCursor = domFn.qsa('.footer-area');
+const footCursor = dFn.qsa('.footer-area');
 // 한번에 셋팅하기
 footCursor.forEach(ele=>{
     ele.onmouseenter = () => mover.style.backgroundColor = '#fff';
