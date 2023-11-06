47dc1d30636e693ec42eea8da8253b0506b59aba
minjxx <qnsdbtlrp1018@naver.com>
2023-11-06 18:20:24 +0900

    up

--- 02.converse_PJ/03.구현소스/js/main.js
+++ 02.converse_PJ/03.구현소스/js/main.js
@@ -1,1 +1,16 @@
 /* main.js */
+$(function () {
+    const welcomSection = $(".welcome-section");
+    const enterButton = welcomSection.find(".enter-button");
+    const mainWrap = $('.wrapper');
+    mainWrap.css('display','none');
+
+    setTimeout(function(){
+        welcomSection.removeClass("content-hidden");
+    }, 800);
+
+    enterButton.on("click", function(){
+        welcomSection.addClass("content-hidden").fadeOut();
+        mainWrap.css('display','');
+    });
+});
