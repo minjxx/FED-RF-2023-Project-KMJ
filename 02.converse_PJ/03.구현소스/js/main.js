e32e3c3fa7ba612d80d3ea14b5f88027d9ebfa1e
minjxx <qnsdbtlrp1018@naver.com>
2023-11-07 12:03:24 +0900

    update

--- 02.converse_PJ/03.구현소스/js/main.js
+++ 02.converse_PJ/03.구현소스/js/main.js
@@ -1,5 +1,7 @@
 /* main.js */
-$(function () {
+
+/* 인트로 나오는 js */
+/* $(function () {
     const welcomSection = $(".welcome-section");
     const enterButton = welcomSection.find(".enter-button");
     const mainWrap = $('.wrapper');
@@ -13,4 +15,38 @@ $(function () {
         welcomSection.addClass("content-hidden").fadeOut();
         mainWrap.css('display','');
     });
-});
+}); */
+
+window.onload = function () {
+    measurePageLoadTime();
+  };
+  
+  function measurePageLoadTime() {
+    var loadTime =
+      window.performance.timing.domContentLoadedEventEnd -
+      window.performance.timing.navigationStart;
+  
+    // 로딩 화면 요소
+    var loadingElement = document.getElementById('loading');
+    var loadingTextElement = document.getElementById('loading-text');
+  
+    // 로딩 수치 업데이트
+    var progress = 0;
+    var increment = 100 / loadTime; // 로딩 시간에 따라 증가하는 진행률 계산
+    var intervalId = setInterval(function () {
+      progress += increment;
+      loadingTextElement.textContent = Math.round(progress) + '%';
+      if (progress >= 100) {
+        clearInterval(intervalId);
+        // main.style.height = '100%';
+        progress = 100; // 진행률(progress)을 100으로 제한
+        loadingTextElement.textContent = Math.round(progress) + '%'; // 수치를 100%로 표시
+        loadingElement.classList.add('hidden'); // 로딩 화면에 hidden 클래스 추가
+        document.body.style.visibility = 'visible'; // 페이지 내용 보이기
+        setTimeout(function () {
+          loadingElement.style.display = 'none'; // 로딩 화면 요소 제거
+          startPage(); // 페이지 실행 코드
+        }, 500);
+      }
+    });
+  }
