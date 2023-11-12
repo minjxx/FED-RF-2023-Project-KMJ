544bd9e61941e4b559668a1a03636ea7c4aa0a2e
minjxx <qnsdbtlrp1018@naver.com>
2023-11-12 23:50:26 +0900

    update

--- 02.converse_PJ/03.구현소스/js/main.js
+++ 02.converse_PJ/03.구현소스/js/main.js
@@ -4,17 +4,3 @@
 // 모듈 불러오기 ///////
 // DOM함수 모듈
 import dFn from "./dom.js";
-
-/*******************************************/
-/* 인트로 화면에서 숨겨져있다가 페이드인하는 js */
-loadIntro();
-
-function loadIntro() {
-    const introFade = $(".intro-fade");
-    //console.log(introFade);
-
-    setTimeout(() => {
-        introFade.fadeIn(1500);
-    }, 1000);
-}
-/*******************************************/
