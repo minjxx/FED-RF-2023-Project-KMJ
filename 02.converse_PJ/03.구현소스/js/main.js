ec3a205aa10d0954084ec09a075b417d4e891785
minjxx <qnsdbtlrp1018@naver.com>
2023-11-21 18:19:28 +0900

    update

--- 02.converse_PJ/03.구현소스/js/main.js
+++ 02.converse_PJ/03.구현소스/js/main.js
@@ -4,6 +4,19 @@
 // DOM함수 모듈
 import dFn from "./dom.js";
 
+$(function() {
+    let tabBtn = $(".tab-btn > ul > li"); //버튼 설정
+    let tabCont = $(".tab-cont > div"); //콘텐츠 설정
+    tabCont.hide().eq(0).show(); //첫번째 콘텐츠만 보이게 설정
+
+    tabBtn.click(function(){
+        const index = $(this).index(); //클릭한 번호를 저장
+        // console.log(index);
+        $(this).addClass("active").siblings().removeClass("active"); //내가 클릭한 버튼에 클래스를 추가하고 나머지 버튼은 삭제
+        tabCont.eq(index).show().siblings().hide(); //내가 클릭한 버튼의 콘텐츠는 보여주고 나머지는 숨김
+    });
+});
+
 /////////////////////////////////////
 /// 인트로 동영상 클릭시 플레이하기 ////
 // 대상: .intro-mv-img
