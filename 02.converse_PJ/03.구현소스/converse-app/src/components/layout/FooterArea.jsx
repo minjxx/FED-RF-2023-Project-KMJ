92aedcb6d6a11eeb4ae569e3d85690b7edc49313
minjxx <qnsdbtlrp1018@naver.com>
2023-11-26 00:08:36 +0900

    update

--- 02.converse_PJ/03.구현소스/converse-app/src/components/layout/FooterArea.jsx
+++ 02.converse_PJ/03.구현소스/converse-app/src/components/layout/FooterArea.jsx
@@ -1,6 +1,77 @@
+// 제이쿼리
+// import $ from 'jquery';
+
+// 폰트어썸 불러오기
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 export function FooterArea(){
   return(
     <>
+      <footer className="footer">
+        <div className="footer-wrap">
+          <div className="footer-left">
+            <div className="footer-logo"><img src="./images/common/logo_white.png" alt="컨버스" /></div>
+            <address>
+              <p>
+                <span>사업자명 : (유)컨버스코리아</span>
+                <span>대표자명 : 이명재</span>
+                <span>사업자등록번호 : 220-88-74818</span>
+              </p>
+              <p>
+                <span>통신판매업 : 제2016-서울강남-00478호</span>
+              </p>
+              <p>
+                <span>주소 : 서울특별시 강남구 테헤란로 152 (역삼동) 강남파이낸스센터 32층</span>
+              </p>
+              <p>
+                <span>TEL : 080-987-0182</span>
+                <span>이용문의 : conversekorea@converse.co.kr</span>
+              </p>
+            </address>
+            <p className="copyright">2020 Converse Korea LLC. All Rights Reserved.</p>
+          </div>
+          <div className="footer-right">
+            <div className="social">
+              {/* 페이스북 아이콘 */}
+              <a href="https://www.facebook.com/converse.kr" target="_blank" title="Facebook">
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              {/* 인스타 아이콘 */}
+              <a href="https://www.instagram.com/converse_kr" target="_blank" title="Instagram">
+                <FontAwesomeIcon icon={faInstagram} />
+              </a>
+            </div>
+            <div className="sitemap">
+              <dl>
+                <dt>support</dt>
+                <dd><a href="#">고객지원센터</a></dd>
+                <dd><a href="#">1:1 문의</a></dd>
+                <dd><a href="#">주문/배송 조회</a></dd>
+              </dl>
+              <dl>
+                <dt>information</dt>
+                <dd><a href="#">컨버스에 대하여</a></dd>
+                <dd><a href="#">회원가입</a></dd>
+                <dd><a href="#">매장안내</a></dd>
+                <dd><a href="#">공지사항</a></dd>
+              </dl>
+              <dl>
+                <dt>policy</dt>
+                <dd><a href="#">이용약관</a></dd>
+                <dd><a href="#">개인정보처리방침</a></dd>
+              </dl>
+              <dl>
+                <dt>family sites</dt>
+                <dd><a href="#">nike</a></dd>
+                <dd><a href="#" className="cap">jordan</a></dd>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </footer>
+      {/* 탑버튼 */}
+      <button className="topBtn"></button>
     </>
   );
 }
--- 02.converse_PJ/03.구현소스/converse-app/src/components/layout/TopArea.jsx
+++ 02.converse_PJ/03.구현소스/converse-app/src/components/layout/TopArea.jsx
@@ -1,90 +1,92 @@
-
-
-export function TopArea(){
-  return(
+export function TopArea() {
+  return (
     <>
-      <header id="header">
-      <div className="header-wrap">
-        <h1 className="logo">
-          <a href="/" className="logo-img"><img src="./images/common/logo_black.png" alt="컨버스" /></a>
-        </h1>
-        <nav className="pc-nav">
-          <ul className="menu-list-wrap">
-            <li className="menu-list">
-              <a href="#">men</a>
-              <div className="subMenu-wrap">
-                <div className="subMenu">
-                  <strong className="depth1_title">men</strong>
-                  <ul className="depth2">
-                    <li className="depth2_list">
-                      <a href="#">ALL</a>
-                    </li>
-                    <li className="depth2_list">
-                      <a href="#">NEW</a>
-                    </li>
-                    <li className="depth2_list">
-                      <a href="#">BEST</a>
-                    </li>
-                  </ul>
+      <header className="header">
+        <div className="header-wrap">
+          <h1 className="logo">
+            <a href="/" className="logo-img">
+              <img src="./images/common/logo_black.png" alt="컨버스" />
+            </a>
+          </h1>
+          <nav className="pc-nav">
+            <ul className="menu-list-wrap">
+              <li className="menu-list">
+                <a href="#">men</a>
+                <div className="subMenu-wrap">
+                  <div className="subMenu">
+                    <strong className="depth1_title">men</strong>
+                    <ul className="depth2">
+                      <li className="depth2_list">
+                        <a href="#">ALL</a>
+                      </li>
+                      <li className="depth2_list">
+                        <a href="#">NEW</a>
+                      </li>
+                      <li className="depth2_list">
+                        <a href="#">BEST</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </li>
-            <li className="menu-list">
-              <a href="#">women</a>
-              <div className="subMenu-wrap">
-                <div className="subMenu">
-                  <strong className="depth1_title">women</strong>
-                  <ul className="depth2">
-                    <li className="depth2_list">
-                      <a href="#">ALL</a>
-                    </li>
-                    <li className="depth2_list">
-                      <a href="#">NEW</a>
-                    </li>
-                    <li className="depth2_list">
-                      <a href="#">BEST</a>
-                    </li>
-                  </ul>
+              </li>
+              <li className="menu-list">
+                <a href="#">women</a>
+                <div className="subMenu-wrap">
+                  <div className="subMenu">
+                    <strong className="depth1_title">women</strong>
+                    <ul className="depth2">
+                      <li className="depth2_list">
+                        <a href="#">ALL</a>
+                      </li>
+                      <li className="depth2_list">
+                        <a href="#">NEW</a>
+                      </li>
+                      <li className="depth2_list">
+                        <a href="#">BEST</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </li>
-            <li className="menu-list">
-              <a href="#">kids</a>
-              <div className="subMenu-wrap">
-                <div className="subMenu">
-                  <strong className="depth1_title">kids</strong>
-                  <ul className="depth2">
-                    <li className="depth2_list">
-                      <a href="#">ALL</a>
-                    </li>
-                    <li className="depth2_list">
-                      <a href="#">NEW</a>
-                    </li>
-                    <li className="depth2_list">
-                      <a href="#">BEST</a>
-                    </li>
-                  </ul>
+              </li>
+              <li className="menu-list">
+                <a href="#">kids</a>
+                <div className="subMenu-wrap">
+                  <div className="subMenu">
+                    <strong className="depth1_title">kids</strong>
+                    <ul className="depth2">
+                      <li className="depth2_list">
+                        <a href="#">ALL</a>
+                      </li>
+                      <li className="depth2_list">
+                        <a href="#">NEW</a>
+                      </li>
+                      <li className="depth2_list">
+                        <a href="#">BEST</a>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
-              </div>
-            </li>
-            <li className="menu-list">
-              <a href="#">event</a>
-            </li>
-            <li className="menu-list">
-              <a href="#">sale</a>
-            </li>
-          </ul>
-        </nav>
-        <div className="top-icon">
-          <div className="top-search">
-            <input type="text" className="top-input-box" placeholder="Search" />
-            <button className="search-icon">검색</button>
+              </li>
+              <li className="menu-list">
+                <a href="#">event</a>
+              </li>
+              <li className="menu-list">
+                <a href="#">sale</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="top-icon">
+            <div className="top-search">
+              <input type="text" className="top-input-box" placeholder="Search" />
+              <button className="search-icon">검색</button>
+            </div>
+            <a href="#" className="user"></a>
+            <a href="#" className="cart">
+              <span className="cart-cnt">0</span>
+            </a>
           </div>
-          <a href="#" className="user"></a>
-          <a href="#" className="cart"><span className="cart-cnt">0</span></a>
         </div>
-      </div>
-    </header>
+      </header>
     </>
   );
 }
