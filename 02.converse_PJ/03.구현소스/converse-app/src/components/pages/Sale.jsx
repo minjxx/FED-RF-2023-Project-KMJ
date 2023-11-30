2df12ac8da2bd1d75a7b49ec858c4c4baa1ea9dd
minjxx <qnsdbtlrp1018@naver.com>
2023-11-30 17:43:48 +0900

    Update Sale.jsx

--- 02.converse_PJ/03.구현소스/converse-app/src/components/pages/Sale.jsx
+++ 02.converse_PJ/03.구현소스/converse-app/src/components/pages/Sale.jsx
@@ -1,8 +1,16 @@
 export function Sale() {
   return (
-    <section className="sub-cont sale-wrap">
+    <section className="sub-area">
       <div className="comm-title">
         <h2>sale</h2>
+      </div>
+      <div className="content-inner">
+        <div className="filter">
+          
+        </div>
+        <div className="list-box">
+          
+        </div>
       </div>
     </section>
   );
